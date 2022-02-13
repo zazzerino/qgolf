ecb435a8b163d09b5841f83c401eb3db6484907e
Kyle Patterson <patterkyle@gmail.com>
2022-02-13 15:49:24 -0600

    db init

--- backend/golf/src/main/java/com/kdp/golf/App.java
+++ backend/golf/src/main/java/com/kdp/golf/App.java
@@ -1,12 +1,41 @@
 package com.kdp.golf;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.event.Observes;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 @QuarkusMain
 public class App {
 
+    private final DataSource dataSource;
+    private final Logger log = Logger.getLogger(App.class);
+
+    public App(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     public static void main(String... args) {
         Quarkus.run(args);
+    }
+
+    void initDatabase(@Observes StartupEvent _e) throws IOException, SQLException {
+        log.info("initializing database...");
+        var schemaPath = Paths.get("src/main/resources/schema.sql");
+
+        try (var lines = Files.lines(schemaPath);
+             var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            var sql = lines.collect(Collectors.joining("\n"));
+            statement.execute(sql);
+        }
     }
 }
